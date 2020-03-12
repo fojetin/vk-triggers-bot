4acee1b250cad81875475ee7920b6c6251c76859
Dmitriy Plaksunov <dmitriy.plaksunov@ostrovok.ru>
2020-03-12 05:06:39 +0300

    Add reg exps

--- botengine/bot.py
+++ botengine/bot.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import random
-
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from vk_api.upload import FilesOpener
+import re
 
 from django.conf import settings
-from .models import Answer, Trigger, RegexTrigger, \
-    TextTrigger, OneTextTrigger
+
+import vk_api
+from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
+
+from botengine.models import Answer, OneTextTrigger, RegexTrigger, TextTrigger, Trigger
 
 
 def get_random_id():
@@ -19,29 +19,39 @@ def bot_handler(raw_event):
     vk_session = vk_api.VkApi(token=settings.VK_BOT_TOKEN)
     vk = vk_session.get_api()
     longpoll = VkBotLongPoll(vk_session, settings.VK_GROUP_ID)
-    
+
     event = longpoll._parse_event(raw_event)
     lazy_logg(event)
-    
+
     if not event.obj.text:
         return
     text = event.obj.text.lower()
 
-    regex_trigger = RegexTrigger.objects.all()
+    regex_triggers = RegexTrigger.objects.all()
     text_triggers = OneTextTrigger.objects.all()
 
-    # reg exps
-    for trigger in [(trigger.reg_exp, trigger.pk) for trigger in regex_trigger]:
-        pass
+    for trigger in regex_triggers:
+        # reg exps
+        match = re.match(trigger.reg_exp, text.lower())
+        if match:
+            answer = trigger.answer
+            break
     else:
         # text exps
-        for trigger in [(trigger.text.lower(), trigger.text_trigger.id, trigger.text_trigger.entry) for trigger in text_triggers]:
+        for trigger in [(trigger.text.lower(),
+                        trigger.text_trigger.id,
+                        trigger.text_trigger.entry)
+                        for trigger
+                        in text_triggers]:
             if trigger[2]:
                 # Вхождение
+                if trigger[0] in text.lower().split():
+                    answer = Answer.objects.get(trigger__pk=trigger[1])
+                    break
                 pass
             else:
                 # Совпадение
-                if trigger[0] == text:
+                if trigger[0].lower() == text.lower():
                     answer = Answer.objects.get(trigger__pk=trigger[1])
                     break
         else:
@@ -67,5 +77,3 @@ def lazy_logg(event):
     print('Новое сообщение:')
     print('От:', event.obj.from_id)
     print('Текст:', event.obj.text)
-
-
